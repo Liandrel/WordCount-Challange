fd3c7a4c83961efd5f68ef10781ad7a28e211a9b
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-12 20:04:29 +0200

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using WordCountLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        private static string[] tests = new string[]
+        {
+            @"The test of the 
+            best way to handle
+
+multiple lines,   extra spaces and more.",
+            @"Using the starter app, create code that will 
+loop through the strings and identify the total 
+character count, the number of characters
+excluding whitespace (including line returns), and the
+number of words in the string. Finally, list each word, ensuring it
+is a valid word."
+        };
+
+        /* 
+            First string (tests[0]) Values:
+            Total Words: 14
+            Total Characters: 89
+            Character count (minus line returns and spaces): 60
+            Most used word: the (2 times)
+            Most used character: e (10 times)
+
+            Second string (tests[1]) Values:
+            Total Words: 45
+            Total Characters: 276
+            Character count (minus line returns and spaces): 230
+            Most used word: the (6 times)
+            Most used character: t (24 times)
+        */
+
+        static void Main(string[] args)
+        {
+            foreach (string test in tests)
+            {
+                WordCounter counter = new WordCounter(test);
+
+                Console.WriteLine("String Values: ");
+                Console.WriteLine($"Total Words: {counter.TotalWords}");
+                Console.WriteLine($"Total Characters: {counter.TotalCharacters}");
+                Console.WriteLine($"Character count: {counter.CharacterCount}");
+                Console.WriteLine($"Most used word: {counter.MostUsedWord.word} ({counter.MostUsedWord.count} times)");
+                Console.WriteLine($"Most used character: {counter.MostUsedCharacter.character} ({counter.MostUsedCharacter.count} times)");
+                Console.WriteLine($"All words: {String.Join(", ", counter.WordsInString)}");
+                Console.WriteLine($"Unique words:{String.Join(", ", counter.UniqueWords)} ");
+                Console.WriteLine($"Unique Characters:{String.Join(", ", counter.UniqueCharacters)} ");
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WordCountLibrary/WordCounter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WordCountLibrary
+{
+    public class WordCounter
+    {
+        public int TotalWords { get; set; }
+        public int TotalCharacters { get; set; }
+        public int CharacterCount { get; set; }
+        public (string word, int count) MostUsedWord { get; set; }
+        public (char character, int count) MostUsedCharacter { get; set; }
+        public List<string> WordsInString { get; set; }
+        public List<string> UniqueWords { get; set; }
+        public List<char> UniqueCharacters { get; set; }
+
+        public WordCounter(string lines)
+        {
+            lines = lines.Replace(Environment.NewLine, " ");
+
+            TotalWords = GetWordsFromString(lines).Count;
+
+            TotalCharacters = CountCharacters(lines);
+
+            CharacterCount = CheckCharacterCount(lines).Count;
+
+            MostUsedWord = CheckMostUsedWord(lines);
+            MostUsedCharacter = CheckMostUsedCharacter(lines);
+            WordsInString = GetWordsFromString(lines).ConvertAll(x=> x.ToLower());
+            UniqueWords = WordsInString.Distinct().ToList();
+            UniqueCharacters = CheckCharacterCount(lines).Distinct().ToList();
+        }
+
+        private (char, int) CheckMostUsedCharacter(string lines)
+        {
+            List<char> chars = CheckCharacterCount(lines);
+            chars = chars.ConvertAll(x => x.ToString().ToLower()[0]);
+
+            var mostOccurances = chars.GroupBy(i => i).OrderByDescending(grp => grp.Count()).First();
+            return (mostOccurances.Key, mostOccurances.Count());
+        }
+
+        private (string, int) CheckMostUsedWord(string lines)
+        {
+            List<string> words = GetWordsFromString(lines);
+            words = words.ConvertAll(x => x.ToLower());
+
+            var mostOccurances = words.GroupBy(i=>i).OrderByDescending(grp => grp.Count()).First();
+            return (mostOccurances.Key,mostOccurances.Count());
+        }
+
+        private List<char> CheckCharacterCount(string lines)
+        {
+            lines = lines.Replace(" ","");
+            return lines.ToCharArray().ToList();
+
+        }
+
+        private int CountCharacters(string lines)
+        {
+            return lines.ToCharArray().Length;
+        }
+
+        private List<string> GetWordsFromString(string lines)
+        {
+            Regex alphaCheck = new Regex("[^a-zA-Z ]");
+            string cleanLines = alphaCheck.Replace(lines, "");
+
+            List<string> words = cleanLines.Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+
+            return words;
+        }
+    }
+}
